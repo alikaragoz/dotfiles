7db4e24b1c26997f3fdc5c2f31a1da371ce2a086
Ali Karagoz <mail@alikaragoz.net>
2014-02-02 20:49:40 +0100

    Add management of custom aliases.

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -9,3 +9,10 @@
 if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 fi
+
+# Load any alias files.
+if [ -d $HOME/.dotfiles/zsh/aliases/ ]; then
+  if [ "$(ls -A $HOME/.dotfiles/zsh/aliases/)" ]; then
+    for config_file ($HOME/.dotfiles/zsh/aliases/*.zsh) source $config_file
+  fi
+fi
